8595a9efa67699b24cb408408c66ee7e24c45d59
Everton Ulisses <everton.mendonca@cassi.com.br>
2022-09-10 23:56:47 -0300

    POST e GET básico

--- src/test/java/apiTests/Pet.java
+++ src/test/java/apiTests/Pet.java
@@ -1,5 +1,9 @@
 package apiTests;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,28 +13,66 @@ import static org.hamcrest.Matchers.*;
 
 public class Pet {
 
-   public String lerJson(String caminhoJson) throws IOException {
-       return new String(Files.readAllBytes(Paths.get(caminhoJson)));
-   }
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
 
     @Test
     public void incluirPet() throws IOException {
-
+        RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
         String jsonBody = lerJson("src/test/resources/data/pet.json");
-
         given()
-                .contentType("applicationJson")
+                .contentType("application/json")
                 .log().all()
-
+                .body(jsonBody)
         .when()
                 .post("https://petstore.swagger.io/v2/pet")
         .then()
                 .log().all() //registrar tudo da volta
                 .statusCode(200)
-                .body("id", is(459))
+                .body("id", is(123459))
                 .body("name", is("spike"))
-                .body("category.name",is("dog"))
-
-        ;
+                .body("category.name", is("dog"));
+        System.out.println("Adicionado com Sucesso"); // + response.getStatusLine());
+        //System.out.println("Response=>" + response.prettyPrint());
     }
+
+    @Test
+    public void listarPet() throws IOException {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.request(Method.GET, "");
+        System.out.println("Status received => " + response.getStatusLine());
+        System.out.println("Response=>" + response.prettyPrint());
+
+    }
+
+    @Test
+    public void consultarPet() {
+        String PetId ="123459";
+        given()
+                .contentType("application/json")
+                .log().all()
+                .when()
+                      .get("https://petstore.swagger.io/v2/pet/" + PetId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("spike"))
+                .body("status",is("available"))
+                ;
+    }
+
+    @Test
+    public void listar2Pet() throws IOException {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("1");
+        //System.out.println("Response Body is:  " + response.asString()); //saida em uma linha.: com prertty sai como o json formatado
+        System.out.println("Response Body is:  " + response.prettyPrint());
+
+
+    }
+
+
 }
