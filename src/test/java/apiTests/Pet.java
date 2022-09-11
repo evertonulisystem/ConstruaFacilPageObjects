c25b773ccfd7fc9131a32ba02b66d4327f8009fa
Everton Ulisses <everton.mendonca@cassi.com.br>
2022-09-10 21:40:28 -0300

    Montando Teste de API com restAssured

--- src/test/java/apiTests/Pet.java
+++ src/test/java/apiTests/Pet.java
@@ -1,4 +1,36 @@
 package apiTests;
 
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class Pet {
+
+   public String lerJson(String caminhoJson) throws IOException {
+       return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+   }
+
+    @Test
+    public void incluirPet() throws IOException {
+
+        String jsonBody = lerJson("src/test/resources/data/pet.json");
+
+        given()
+                .contentType("applicationJson")
+                .log().all()
+
+        .when()
+                .post("https://petstore.swagger.io/v2/pet")
+        .then()
+                .log().all() //registrar tudo da volta
+                .statusCode(200)
+                .body("id", is(459))
+                .body("name", is("spike"))
+                .body("category.name",is("dog"))
+
+        ;
+    }
 }
