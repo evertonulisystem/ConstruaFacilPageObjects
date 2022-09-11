a3686e7ae615b4babffdef14e518fc21875d3ff6
Everton Ulisses <everton.mendonca@cassi.com.br>
2022-09-11 16:29:04 -0300

    Evidencias em casos de testes

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -1,4 +1,44 @@
 package webTests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.Evidencias;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class seleniumSimples {
+    //Atributos
+    WebDriver driver;
+    Evidencias evidencias;
+
+
+    //Métodos e Funções
+
+
+    @BeforeMethod
+    public void iniciar(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+
+        evidencias = new Evidencias();
+    }
+
+    @Test
+    public void consultarCurso() throws IOException {
+    String casoDeTeste = "Consulta Iterasys";
+        driver.get("https://iterasys.com.br/pt");
+        evidencias.print(driver, casoDeTeste, "Passo1 -Abriu o site");
+
+
+        driver.findElement(By.className("item-pill")).click();
+        evidencias.print(driver, casoDeTeste, "Passo2 -Exibiu o site");
+
+    }
+
 }
